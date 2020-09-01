dd37b72934fe200995af019d15fa93c82596cfd6
李丹丹email <2690905951@qq.com>
2020-09-01 19:02:57 +0800

    模板

---
+++ 08-template模板/template的基本使用/pages/index/index.js
@@ -0,0 +1,16 @@
+Page({
+  data:{
+    mydata:{
+      a:1,
+      b:2
+    }
+  },
+  add(){
+    this.setData({
+      mydata:{
+        ...this.data.mydata,
+        a:this.data.mydata.a +1,
+      }
+    });
+  }
+});
